f5178814c24a171ac42ec8ad52904fced57e9d40
mplibunao <mark.libunao@shoresuite.com>
2017-11-23 18:27:18 +0800

    Can drop only on valid cells; show color highlighting depending on state

--- src/Board/index.js
+++ src/Board/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Knight from "./Knight";
+import BoardSquare from "./BoardSquare";
 import Square from "./Square";
 import { moveKnight, canMoveKnight } from "../Game";
 import { DragDropContext } from 'react-dnd';
@@ -16,23 +17,19 @@ class Board extends Component {
     renderSquare(i) {
         const x = i % 8;
         const y = Math.floor(i/8);
-        const black = (x + y) % 2 === 1;
-
-        const [knightX, knightY] = this.props.knightPosition;
-        const piece = (x === knightX && knightY === y) ?
-            <Knight /> :
-            <div>{`[${x}, ${y}]`}</div>;
-        
         return (
-            <div key={i}
-                style={{ width: '12.5%', height: '12.5%' }}
-                onClick={() => this.handleSquareClick(x, y)}
-            >
-                <Square black={black}>
-                    {piece}
-                </Square>
+            <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
+                <BoardSquare x={x} y={y}>
+                    {this.renderPiece(x, y)}
+                </BoardSquare>
             </div>
         );
+    }
+
+    renderPiece(x, y) {
+        const [knightX, knightY] = this.props.knightPosition;
+		const isKnightHere = x === knightX && y === knightY;
+		return isKnightHere ? <Knight /> : <div>1</div>;
     }
 
     handleSquareClick(toX, toY) {
@@ -48,7 +45,7 @@ class Board extends Component {
             squares.push(this.renderSquare(i));
         }
         return (
-            <div style={{
+            <div className="Board" style={{
                 width: '100%',
                 height: '100%',
                 display: 'flex',
